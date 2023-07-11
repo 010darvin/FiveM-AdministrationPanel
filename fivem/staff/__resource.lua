54359edc6edfa23761b56728abb0c4d68c1d5243
010darvin <133992859+010darvin@users.noreply.github.com>
2023-07-11 09:54:03 +0200

    Update __resource.lua

--- fivem/staff/__resource.lua
+++ fivem/staff/__resource.lua
@@ -1,1 +1,19 @@
 server_script 'server.lua'
+start easy admin_script 
+start tx admin_script 
+start esx_fire_script 
+start esx_doorlokc_scropt 
+start esx_job_script 
+start esx_jd scriptt
+start car lepd peck
+start car lefd peck
+start car hls peck 
+start car rols rus peck 
+start car pagani peck 
+start car adui peck 
+start car bmw peck
+start car ferrari peck 
+start car labugini peck 
+start car fort peck 
+start car cedileg peck 
+start car dodger peck 
